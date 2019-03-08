470de1b37702c908aad3e7307d9eaf5500c1f8e9
sacrament <xkinked.tailx@gmail.com>
2019-03-08 14:44:54 +0900

    お気に入り一覧表示機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def look_all
+    @posts = current_user.likes_posts
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     collection do
       post 'confirm'
     end
+    member do
+      get 'look_all'
+    end
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :posts do
